6df30c1fd1d9c7b6880e5227ecc4d66174a29c76
cjb129 <cjb129@zips.uakron.edu>
2013-09-05 11:25:16 -0400

    Added String Task

---
+++ codeforces/StringTask.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<ctype.h>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+	string input;
+	cin >> input;
+	char c;
+	//input.tolower();
+
+	for (int i = 0; i < input.size(); i++) {
+		c = input[i];
+		//cout << ".";
+		//c.tolower();
+		if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='y'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||c=='Y') {
+			continue;
+		}
+		else {
+			cout << ".";
+			putchar(tolower(c));
+		}
+
+	}
+
+	//system("pause");
+
+
+
+	return 0;
+}
