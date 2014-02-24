901ab69208da35e379614fbc62f5cbea07ec4cd5
cjb129 <cjb129@zips.uakron.edu>
2014-02-23 21:18:10 -0500

    Create UltraFastMath.cpp

---
+++ codeforces/UltraFastMath.cpp
@@ -0,0 +1,32 @@
+//Ultra Fast Mathemitician, solved 2/22/14
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    string n, m;
+
+    cin >> n >> m;
+    vector<int> foo;
+
+    for(int i = 0; i < n.length(); i++) {
+        if(n[i] == m[i]) {
+            foo.push_back(0);
+        }
+        else {
+            foo.push_back(1);
+        }
+    }
+
+    for(int i = 0; i < foo.size(); i++) {
+        cout << foo[i];
+    }
+
+
+    //system("pause");
+    return 0;
+}
