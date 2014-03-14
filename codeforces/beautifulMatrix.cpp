535c6f55143930806dc441613ed34e1ab7c039ed
cjb129 <cjb129@zips.uakron.edu>
2014-03-14 19:38:11 -0400

    Create beautifulMatrix.cpp

---
+++ codeforces/beautifulMatrix.cpp
@@ -0,0 +1,39 @@
+//Beautful Matrix, solved 3/14/14
+//http://codeforces.com/problemset/problem/263/A
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+
+	int ar[5][5];
+
+	int nums;
+
+	int eye, jay;
+
+	for(int i = 0; i < 5; i++) {
+		for(int j = 0; j < 5; j++) {
+			cin >> nums;
+			if(nums == 1) {
+				eye = i+1;
+				jay = j+1;
+			}
+		}
+	}
+
+
+	jay = abs(jay-3);
+	eye = abs(eye-3);
+
+	int foo = jay + eye;
+
+	cout << foo;
+
+
+
+	system("pause");
+	return 0;
+}
